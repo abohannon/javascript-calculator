acfd2981832dada80abe9ce8abf64dccf804a57a
abohannon <abo46n2@gmail.com>
2017-06-29 15:35:03 -0700

    Added click event, printing values of buttons to entry box.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
+var entryValue = "0";
 
-  $('button').click(function(){
+document.querySelector("#entry").innerHTML = entryValue;
 
-    $(this).attr("value");
-    console.log(this);
+var buttonsArray = Array.from(document.getElementsByTagName("button"));
+
+buttonsArray.filter(function(element){
+  element.addEventListener("click", function(e){
+
+    console.log(e.target.value);
+    console.log(this.value);
+
+    var newEntryValue = this.value;
+
+    document.querySelector("#entry").innerHTML = newEntryValue;
+
   });
-
-
-
-
-
-
-
-
-
-});
+}, false);
