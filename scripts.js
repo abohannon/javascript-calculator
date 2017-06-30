818c164fdd1a5d29e66af9170d308cc21a7ddcbf
abohannon <abo46n2@gmail.com>
2017-06-29 22:06:25 -0700

    More js work and some css positioning of buttons

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
 var entryValue = "0";
-
 document.querySelector("#entry").innerHTML = entryValue;
+
+var newEntryValue = [];
 
 var buttonsArray = Array.from(document.getElementsByTagName("button"));
 
-buttonsArray.filter(function(element){
-  element.addEventListener("click", function(e){
 
-    console.log(e.target.value);
-    console.log(this.value);
+buttonsArray.filter(function(element) {
 
-    var newEntryValue = this.value;
+  if (element.value !== "ac" && element.value !== "ce") {
 
-    document.querySelector("#entry").innerHTML = newEntryValue;
+  element.addEventListener("click", function(e) {
+
+    // var entryValue = this.value;
+    newEntryValue.push(this.value);
+
+    document.querySelector("#entry").innerHTML = newEntryValue.join("");
 
   });
+
+}
+
+
 }, false);
